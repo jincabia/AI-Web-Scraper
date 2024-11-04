933cb43cb824fa0d9708335aa42f6efad8c9de6b
jincabia <140627796+jincabia@users.noreply.github.com>
2024-11-04 13:35:37 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from scrape import (
+    scrape_website,
+    split_dom_content,
+    extract_body_content,
+    clean_body_content
+)
+from parse import parse_with_ollama
+st.title("AI Web Scraper")
+url = st.text_input("Enter a website URL: ")
+
+if st.button("Scrape Site"):
+    st.write("Scraping the website...")
+    result = scrape_website(url)
+
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_desc = st.text_area("Describe what you want parsed?")
+
+    if st.button("Parse Content"):
+        if parse_desc:
+            st.write("Parsing content...")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks,parse_desc)
+            st.write(result)
